e5819ee5766530039685d94b8c2973fefa44e5b9
Max Holman <max@holmn.com>
2021-03-12 18:34:38 +0800

    feat: favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -123,6 +123,23 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>jwt.one</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
 
       <main className={styles.main}>
