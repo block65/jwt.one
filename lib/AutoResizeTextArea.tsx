370773f35666bc2cd7ef03279c1af072858eaecc
Max Holman <max@holmn.com>
2021-03-12 23:27:37 +0800

    fix: resize textareas on window resize

--- lib/AutoResizeTextArea.tsx
+++ lib/AutoResizeTextArea.tsx
@@ -1,4 +1,4 @@
-import { FC, TextareaHTMLAttributes, useEffect, useRef } from 'react';
+import { FC, TextareaHTMLAttributes, useEffect, useRef, useState } from 'react';
 
 export const AutoResizeTextArea: FC<
   TextareaHTMLAttributes<HTMLTextAreaElement>
@@ -6,12 +6,33 @@ export const AutoResizeTextArea: FC<
   const ref = useRef(null);
   const { value } = props;
 
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   useEffect(() => {
     if (ref.current) {
       ref.current.style.height = 'inherit';
       ref.current.style.height = `${ref.current.scrollHeight}px`;
     }
-  }, [value, ref.current]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value, windowSize]);
 
   return <textarea ref={ref} {...props} />;
 };
